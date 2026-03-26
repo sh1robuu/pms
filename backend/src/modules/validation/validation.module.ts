2727247354bf0f69d78d769eabd732a8f629ccaf
sh1robuu <nampqfhl32249@gmail.com>
2026-03-26 21:20:31 +0700

    refactor: move Next.js app to repo root for Vercel deployment

--- backend/src/modules/validation/validation.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { ValidationService } from './validation.service';
-
-@Global()
-@Module({
-    providers: [ValidationService],
-    exports: [ValidationService],
-})
-export class ValidationModule { }
